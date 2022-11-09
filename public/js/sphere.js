d768b3f1e42ee95413696d6d00a2116b2d271b55
May Carandang <maytiara@gmail.com>
2022-11-09 16:37:20 +0800

    added camera, scene, and renderer using different parameters to specify the value per each

--- public/js/sphere.js
+++ public/js/sphere.js
@@ -1,2 +1,32 @@
-import * as Three from "/build/three.module.js";
-console.log(Three);
+import * as THREE from "/build/three.module.js";
+console.log(THREE);
+
+let scene, camera, renderer, controls;
+const container = document.querySelector(".web-gl");
+
+const init = () => {
+  // scene
+  scene = new THREE.Scene();
+
+  // camera
+  const angle = 40; //wide-angle lens
+  const aspect = window.innerWidth / window.innerHeight; //aspect ratio of the window
+  const near = 0.1;
+  const distance = 1000; // distance of the model shouldn't be greater than before value
+
+  camera = new THREE.PerspectiveCamera(angle, aspect, near, distance);
+  camera.position.set(0, 0, 25); // x,y,z
+  //console.log(camera); //for checking the value
+
+  // renderer
+  renderer = new THREE.WebGLRenderer({
+    canvas: container
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1) //pixel ration of the window : add a dynamic statement using ternary
+  //renderer.autoClear = false;
+  renderer.setClearColor(0x000000, 1.0); //default should start at 0x
+  renderer.render(scene, camera);
+}
+
+init();
